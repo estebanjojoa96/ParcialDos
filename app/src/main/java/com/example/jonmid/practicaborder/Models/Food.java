5d53a2d241a51b7803a19ec40edd183e638589dd
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-04-26 12:38:41 -0500

    commit 2

--- app/src/main/java/com/example/jonmid/practicaborder/Models/Food.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Models/Food.java
@@ -1,5 +1,13 @@
 package com.example.jonmid.practicaborder.Models;
 
 public class Food {
-    
+    public String getTitle() {
+        return Title;
+    }
+
+    public void setTitle(String title) {
+        Title = title;
+    }
+
+    private String Title;
 }
