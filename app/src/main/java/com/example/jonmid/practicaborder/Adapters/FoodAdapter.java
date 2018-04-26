219d669a8d14956720813b554d76705a07af89e0
Esteban_Dario_Jojoa_Rosero <estebandariojojoa9@gmail.com>
2018-04-26 13:28:20 -0500

    commit 3

--- app/src/main/java/com/example/jonmid/practicaborder/Adapters/FoodAdapter.java
+++ app/src/main/java/com/example/jonmid/practicaborder/Adapters/FoodAdapter.java
@@ -1,4 +1,66 @@
 package com.example.jonmid.practicaborder.Adapters;
 
-public class FoodAdapter {
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.jonmid.practicaborder.Models.Food;
+import com.example.jonmid.practicaborder.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FoodAdapter extends RecyclerView.Adapter<FoodAdapter.ViewHolder> {
+
+    List<Food> FoodList = new ArrayList<>();
+    Context context;
+
+    public FoodAdapter(List<Food> FoodList, Context context) {
+        this.FoodList = FoodList;
+        this.context = context;
+    }
+
+    // ******************************************************************************
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        // Obtener la vista (item.xml)
+        View item = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_food, parent, false);
+
+        // Pasar la vista (item.xml) al ViewHolder
+        ViewHolder viewHolder = new ViewHolder(item_food);
+
+        return viewHolder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        // Asignar los valores a la vista
+        holder.textViewFood.setText(FoodList.get(position).getTitle());
+
+
+        @Override
+        public int getItemCount () {
+            return FoodList.size();
+        }
+
+        // ******************************************************************************
+
+        public class ViewHolder extends RecyclerView.ViewHolder {
+            TextView textViewFood;
+
+
+            public ViewHolder(View item) {
+                super(item);
+
+                textViewFood = (TextView) item.findViewById(R.id.id_txv_food_title);
+
+            }
+        }
+    }
 }
--- app/src/main/java/com/example/jonmid/practicaborder/MainActivity.java
+++ app/src/main/java/com/example/jonmid/practicaborder/MainActivity.java
@@ -1,15 +1,132 @@
 package com.example.jonmid.practicaborder;
 
+import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.jonmid.practicaborder.Adapters.FoodAdapter;
+import com.example.jonmid.practicaborder.Http.UrlManager;
+import com.example.jonmid.practicaborder.Models.Food;
+import com.example.jonmid.practicaborder.Parser.JsonFood;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView textViewFood;
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textViewFood = (TextView) findViewById(R.id.id_txv_food_title);
+        recyclerView = (RecyclerView) findViewById(R.id.id_rcv_food);
+
+        // Establcer la orientacion de RecyclerView
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+        recyclerView.setLayoutManager(linearLayoutManager);
+    }
+
+    List<Food> FoodList = new ArrayList<>();
+
+
+    // Metodo para validar la conexion a internet
+    public Boolean isOnLine(){
+        // Hacer llamado al servicio de conectividad utilizando el ConnectivityManager
+        ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        // Obtener el estado de la conexion a internet en el dispositivo
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+
+        // Validar el estado obtenido de la conexion
+        if (networkInfo != null){
+            return true;
+        }else {
+            return false;
+        }
+    }
+
+
+    // Evento del boton
+    public void loadData(View view){
+        if (isOnLine()){
+            // Hacer llamado a la tarea
+            //MyTask task = new MyTask();
+            //task.execute("https://jsonplaceholder.typicode.com/posts");
+
+            MyTask taskFood = new MyTask();
+            MyTask.execute("http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3");
+        }else {
+            Toast.makeText(this, "Sin conexion", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    public void processData(){
+
+
+        FoodAdapter = new FoodAdapter(FoodList, getApplicationContext());
+        recyclerView.setAdapter(FoodAdapter);
+    }
+
+
+
+
+    // Tarea para traer los datos de post
+    public class MyTask extends AsyncTask<String, String, String> {
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected String doInBackground(String... strings) {
+
+
+            String content = null;
+            try {
+                content = UrlManager.getDataJson(strings[0]);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return content;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values) {
+            super.onProgressUpdate(values);
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            try {
+                FoodList = JsonFood.getData(s);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+
+            processData();
+
+
+        }
     }
 }
